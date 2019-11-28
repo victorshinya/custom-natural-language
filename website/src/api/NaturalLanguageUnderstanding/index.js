7ec760063301c86270fd44e9c022ae90639a9477
Victor Shinya <contato@victorshinya.com.br>
2019-11-28 18:28:39 -0300

    Fix API URL logic

--- website/src/api/NaturalLanguageUnderstanding/index.js
+++ website/src/api/NaturalLanguageUnderstanding/index.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const API_ROOT = process.env.NODE_ENV === 'prod' ? '' : 'http://localhost:3001';
-
 const Analyze = (text) => {
-    return axios.post(API_ROOT + '/api/analyze', { text: text })
+    return axios.post('/api/analyze', { text: text })
 }
 
 export default {
